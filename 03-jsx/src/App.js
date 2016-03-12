cacdbe91720aeeca19b087514c2de69c871087e5
velopert <public.velopert@gmail.com>
2016-03-12 18:45:34 +0900

    03-jsx

---
+++ 03-jsx/src/App.js
@@ -0,0 +1,14 @@
+/*jshint esversion: 6 */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    render(){
+
+        return (
+                <h1>Hello Velopert</h1>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
