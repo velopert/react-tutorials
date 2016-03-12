fb81bc96d62c77e0efd5eba4a52ec8d8c2e3e6d4
velopert <public.velopert@gmail.com>
2016-03-12 21:13:07 +0900

    .

--- 03-jsx/src/App.js
+++ 03-jsx/src/App.js
@@ -3,10 +3,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    render(){
+    _sayHey(){
+       alert("hey");
+    }
 
-        return (
-                <h1>Hello Velopert</h1>
+    render(){
+        let text = "Dev-Server";
+
+        let pStyle = {
+            color: 'aqua',
+            backgroundColor: 'black'
+        };
+
+        return  (
+            <div>
+                <h1> Hello Velopert </h1>
+                <h2> Welcome to {text}</h2>
+                <button onClick= {this._sayHey}>Click Me</button>
+                <p style = {pStyle}>{1 == 1 ? 'True' : 'False'}</p>
+        
+            { /* COMMENT */  }
+            </div>
         );
     }
 }
