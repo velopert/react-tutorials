9a8cae4e03bde143c172e20e49ac2fe4e011b612
velopert <public.velopert@gmail.com>
2016-03-21 03:37:24 +0900

    .

--- 04-component/src/App.js
+++ 04-component/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './Header';
+import Content from './Content';
 
 class App extends React.Component {
     render(){
@@ -8,17 +9,6 @@ class App extends React.Component {
             <div>
                 <Header/>
                 <Content/>
-            </div>
-        );
-    }
-}
-
-class Content extends React.Component {
-    render(){
-        return (
-            <div>
-                <h2>Content</h2>
-                <p> Hey! </p>
             </div>
         );
     }
