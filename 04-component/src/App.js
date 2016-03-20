4d23493a1c7134d0a3dacfdb088ccd2cf28eb8a3
velopert <public.velopert@gmail.com>
2016-03-21 02:44:08 +0900

    .

--- 04-component/src/App.js
+++ 04-component/src/App.js
@@ -2,16 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-
-
     render(){
-
         return  (
             <div>
-                Hey
+                <Header/>
+                <Content/>
             </div>
         );
     }
 }
+
+class Header extends React.Component{
+    render(){
+        return (
+            <h1>Header</h1>
+        );
+    }
+}
+
+class Content extends React.Component{
+    render(){
+        return (
+            <div>
+                <h2>Content</h2>
+                <p> Hey! </p>
+            </div>
+        );
+    }
+}
+
+
 
 ReactDOM.render(<App />, document.getElementById('app'));
