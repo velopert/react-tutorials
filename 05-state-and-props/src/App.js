5e1f6fed7fd81be44befb2d2128d7735b0804e95
velopert <public.velopert@gmail.com>
2016-03-21 19:55:50 +0900

    .

---
+++ 05-state-and-props/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './Header';
+import Content from './Content';
+
+class App extends React.Component {
+    render(){
+        return  (
+            <div>
+                <Header/>
+                <Content/>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
