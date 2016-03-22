e7cb676c94e8e771eca86fbc8a41787a0cdae922
velopert <public.velopert@gmail.com>
2016-03-22 23:54:35 +0900

    .

--- 05-state-and-props/src/RandomNumber.js
+++ 05-state-and-props/src/RandomNumber.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
-
+function myFunction(){
+    console.log(this.props.number);
+}
 class RandomNumber extends React.Component {
-
-
     _update(){
         let value = Math.round(Math.random()*100);
         this.props.onUpdate(value);
@@ -13,14 +12,14 @@ class RandomNumber extends React.Component {
 
     constructor(props){
         super(props);
+        this.myFunction = myFunction.bind(this);
     }
-
 
     render(){
         return (
             <div>
                 <h1>RANDOM NUMBER: { this.props.number }</h1>
-                <button onClick={this._update.bind(this)}>Randomize</button>
+                <button onClick={this.myFunction}>Randomize</button>
             </div>
         );
     }
