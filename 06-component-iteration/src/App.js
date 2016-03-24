e2c5b0769f6c3d4821a1aa31615e754352f4f2e0
velopert <public.velopert@gmail.com>
2016-03-24 22:44:51 +0900

    .

---
+++ 06-component-iteration/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    render(){
+
+        return (
+                <Contacts/>
+        );
+    }
+}
+
+class Contacts extends React.Component {
+    render(){
+        return(
+            <div>
+                <h1>Contacts</h1>
+                <ul>
+                    <ContactInfo name="Abet" phone="010-0000-0001"/>
+                    <ContactInfo name="Betty" phone="010-0000-0002"/>
+                    <ContactInfo name="Charlie" phone="010-0000-0003"/>
+                    <ContactInfo name="David" phone="010-0000-0004"/>
+                </ul>
+            </div>
+        );
+    }
+}
+
+class ContactInfo extends React.Component {
+    render() {
+        return(
+            <li>{this.props.name} {this.props.phone}</li>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
