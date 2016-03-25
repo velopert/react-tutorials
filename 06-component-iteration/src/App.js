e62bdb3d9fd6ae5ad72a1c02585b836c6246c1f4
velopert <public.velopert@gmail.com>
2016-03-25 12:53:39 +0900

    .

--- 06-component-iteration/src/App.js
+++ 06-component-iteration/src/App.js
@@ -11,15 +11,26 @@ class App extends React.Component {
 }
 
 class Contacts extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            contactData: [
+                {name: "Abet", phone: "010-0000-0001"},
+                {name: "Betty", phone: "010-0000-0002"},
+                {name: "Charlie", phone: "010-0000-0003"},
+                {name: "David", phone: "010-0000-0004"}
+            ]
+        };
+    }
     render(){
         return(
             <div>
                 <h1>Contacts</h1>
                 <ul>
-                    <ContactInfo name="Abet" phone="010-0000-0001"/>
-                    <ContactInfo name="Betty" phone="010-0000-0002"/>
-                    <ContactInfo name="Charlie" phone="010-0000-0003"/>
-                    <ContactInfo name="David" phone="010-0000-0004"/>
+                    {this.state.contactData.map((contact, i) => {
+                        return (<ContactInfo name={contact.name}
+                                            phone={contact.phone}/>);
+                    })}
                 </ul>
             </div>
         );
