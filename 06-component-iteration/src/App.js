ab8ebd10b1471d331cfc960bc8b52b3306db7a28
velopert <public.velopert@gmail.com>
2016-03-27 03:53:15 +0900

    .

--- 06-component-iteration/src/App.js
+++ 06-component-iteration/src/App.js
@@ -99,7 +99,7 @@ class ContactInfo extends React.Component {
         };
 
         return(
-            <li style={getStyle()} onClick={this.handleClick.bind(this)}>{this.props.name} {this.props.phone} {this.props.isSelected}</li>
+            <li style={getStyle(this.props.isSelected)} onClick={this.handleClick.bind(this)}>{this.props.name} {this.props.phone} {this.props.isSelected}</li>
             );
     }
 }
