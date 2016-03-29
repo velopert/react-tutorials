042dc39131788adb3fd5a1191feab21c991d12bf
velopert <public.velopert@gmail.com>
2016-03-29 18:30:54 +0900

    .

--- 08-refs/src/App.js
+++ 08-refs/src/App.js
@@ -1,1 +1,14 @@
-asdf
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    render(){
+        return  (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
