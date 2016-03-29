70142d4239ca886e30168094bc6a8d04245645de
velopert <public.velopert@gmail.com>
2016-03-29 19:16:33 +0900

    .

--- 08-refs/src/App.js
+++ 08-refs/src/App.js
@@ -5,7 +5,8 @@ class App extends React.Component {
     render(){
         return  (
             <div>
-
+                <input type="text"></input>
+                <button>Clear n Reset </button>     
             </div>
         );
     }
