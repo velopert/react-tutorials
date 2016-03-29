0303576c5cd3b598bea39f33b66605ce92e745a9
velopert <public.velopert@gmail.com>
2016-03-29 18:49:26 +0900

    .

--- 08-refs/src/App.js
+++ 08-refs/src/App.js
@@ -7,6 +7,7 @@ class App extends React.Component {
             <div>
                 <input></input>
                 <button></button>
+                FUCK
             </div>
         );
     }
