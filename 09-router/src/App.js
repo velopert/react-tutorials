6efbb6da97e75f3c0c30647e6bbcb411b019fcc0
velopert <public.velopert@gmail.com>
2016-04-03 02:18:22 +0900

    .

--- 09-router/src/App.js
+++ 09-router/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router';
 
 class App extends React.Component {
     render(){
 
         return (
-                <h1>Hello Velopert</h1>
+                <h1>Hello React Skeleton</h1>
         );
     }
 }
