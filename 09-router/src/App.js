2d72826d5e4bf169bc755e4c805c450896a11122
velopert <public.velopert@gmail.com>
2016-04-03 02:19:01 +0900

    .

--- 09-router/src/App.js
+++ 09-router/src/App.js
@@ -6,7 +6,7 @@ class App extends React.Component {
     render(){
 
         return (
-                <h1>Hello React Skeleton</h1>
+                <h1>Hello React Skeleton!</h1>
         );
     }
 }
