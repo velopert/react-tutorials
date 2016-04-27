68711d4197e2f276bd0c4df084484570b596de72
velopert <public.velopert@gmail.com>
2016-04-27 13:23:28 +0900

    .

--- 10-redux/src/App.js
+++ 10-redux/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { connect } from 'react-redux';
+import { addCounter, minusCounter } from './actions/actions';
+
 class App extends React.Component {
     render(){
-
+        const { onIncrement, counter } = this.props;
         return (
-                <h1>Hello React Skeleton</h1>
+            <h1 onClick = { onIncrement }>Hello React Skeleton {counter}</h1>
         );
     }
+
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+export default App;
+
+
+
+
